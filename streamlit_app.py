dec7ff5d111765698036cbbfbe5d19bb98699746
Euellbehappy <jeuell@gmail.com>
2023-01-29 17:41:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ sl.text('🥑🍞 Avocado Toast')
    
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt', index = False)
-sl.dataframe(my_fruit_list)
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+sl.dataframe(my_fruit_list, index = False)
