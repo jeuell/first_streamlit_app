5d6f4d071f7ad9d3eceaa0785dabd73b4e7494b9
Euellbehappy <jeuell@gmail.com>
2023-01-29 17:27:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 
 import streamlit
-streamlit.title('My Healthy Diner')
+streamlit.title("My Mom's Healthy Diner")
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
