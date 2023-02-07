082b843baf89b8a05be48a1c48da487ba3748f04
Euellbehappy <jeuell@gmail.com>
2023-02-07 16:26:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ sl.dataframe(fruits_to_show)
 
 sl.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-sl.text(fruityvice_response)
+sl.text(fruityvice_response.json())
 
 # Display the table on the page.
 
